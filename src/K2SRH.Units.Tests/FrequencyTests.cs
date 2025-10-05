a5e0d81a789ec13b1fff49a159b23d53f2e37123
Seth Hendrick <seth@shendrick.net>
2025-10-05 16:34:02 -0400

    Added sub-units for Frequency.

--- src/K2SRH.Units.Tests/FrequencyTests.cs
+++ src/K2SRH.Units.Tests/FrequencyTests.cs
@@ -10,13 +10,119 @@ namespace K2SRH.Units.Tests
     [TestClass]
     public sealed class FrequencyTests
     {
+        // ---------------- Tests ----------------
+
         [TestMethod]
         public void AddTest()
         {
-            Frequency f = new Frequency();
-            Frequency f2 = new Frequency();
+            // Setup
+            Frequency f1 = new Hertz( 100 );
+            Frequency f2 = new Hertz( 200 );
 
-            Frequency f3 = f + f2;
+            // Act
+            Frequency actualFreq = f1 + f2;
+
+            // Check
+            Assert.AreEqual( new Frequency( 300 ), actualFreq );
+            Assert.AreEqual( new Hertz( 300 ) , actualFreq );
+            Assert.AreEqual( new KiloHertz( 0.3m ), actualFreq );
+            Assert.AreEqual( new MegaHertz( 0.0003m ), actualFreq );
+            Assert.AreEqual( new GigaHertz( 0.0000003m ), actualFreq );
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            // Setup
+            Frequency f1 = new KiloHertz( 1 );
+            Frequency f2 = new Hertz( 200 );
+
+            // Act
+            Frequency actualFreq = f1 - f2;
+
+            // Check
+            Assert.AreEqual( new Frequency( 800 ), actualFreq );
+            Assert.AreEqual( new Hertz( 800 ), actualFreq );
+            Assert.AreEqual( new KiloHertz( 0.8m ), actualFreq );
+            Assert.AreEqual( new MegaHertz( 0.0008m ), actualFreq );
+            Assert.AreEqual( new GigaHertz( 0.0000008m ), actualFreq );
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            // Setup
+            Frequency f1 = new KiloHertz( 1 );
+            Frequency f2 = new KiloHertz( 1 );
+
+            // Act
+            Frequency actualFreq = f1 * f2;
+
+            // Check
+            Assert.AreEqual( new Frequency( 1000000 ), actualFreq );
+            Assert.AreEqual( new Hertz( 1000000 ), actualFreq );
+            Assert.AreEqual( new KiloHertz( 1000 ), actualFreq );
+            Assert.AreEqual( new MegaHertz( 1 ), actualFreq );
+            Assert.AreEqual( new GigaHertz( 0.001m ), actualFreq );
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            // Setup
+            Frequency f1 = new GigaHertz( 1 );
+            Frequency f2 = new MegaHertz( 500 );
+
+            // Act
+            Frequency actualFreq = f1 / f2;
+
+            // Check
+            Assert.AreEqual( new Frequency( 2 ), actualFreq );
+            Assert.AreEqual( new Hertz( 2 ), actualFreq );
+            Assert.AreEqual( new KiloHertz( 0.002m ), actualFreq );
+            Assert.AreEqual( new MegaHertz( 0.000002m ), actualFreq );
+            Assert.AreEqual( new GigaHertz( 0.000000002m ), actualFreq );
+
+            Assert.AreEqual( new Hertz( 2 ), actualFreq );
+        }
+
+        [TestMethod]
+        public void AbsTest()
+        {
+            // Setup
+            Frequency f1 = new GigaHertz( -1 );
+            Frequency f2 = new MegaHertz( 500 );
+
+            // Act
+            Frequency actualF1 = Frequency.Abs( f1 );
+            Frequency actualF2 = Frequency.Abs( f2 );
+
+            // Check
+            Assert.AreEqual( new GigaHertz( 1 ), actualF1 );
+            Assert.AreEqual( new GigaHertz( 0.5m ), actualF2 );
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            void DoTest( Frequency freq, string expectedString )
+            {
+                Assert.AreEqual( expectedString, freq.ToString() );
+            }
+
+            DoTest( new Hertz( 1 ), "1 Hz" );
+            DoTest( new Hertz( 100 ), "100 Hz" );
+            DoTest( new Hertz( 999.999m ), "999.999 Hz" );
+
+            DoTest( new Hertz( 1000 ), "1 KHz" );
+            DoTest( new KiloHertz( 999 ), "999.0 KHz" );
+
+            DoTest( new KiloHertz( 1000 ), "1.0 MHz" );
+            DoTest( new MegaHertz( 1 ), "1.0 MHz" );
+            DoTest( new MegaHertz( 999 ), "999.0 MHz" );
+
+            DoTest( new MegaHertz( 1000 ), "1.0 GHz" );
+            DoTest( new GigaHertz( 2 ), "2.0 GHz" );
         }
     }
 }
---
+++ src/K2SRH.Units/Frequency/GigaHertz.cs
@@ -0,0 +1,41 @@
+﻿namespace K2SRH.Units
+{
+    public struct GigaHertz
+    {
+        // ---------------- Constructor ----------------
+
+        public GigaHertz( decimal gigaHertz )
+        {
+            this.Value = gigaHertz;
+        }
+
+        public static implicit operator Frequency( GigaHertz gigaHertz )
+        {
+            return new Frequency( gigaHertz.Value * 1000.0m * 1000m * 1000m );
+        }
+
+        // ---------------- Properties ----------------
+
+        public decimal Value { get; }
+
+        // ---------------- Methods ----------------
+
+        public override string ToString()
+        {
+            return $"{this.Value} GHz";
+        }
+    }
+
+    public static class GigaHertzExtensions
+    {
+        public static decimal GigaHertz( this Frequency frequency )
+        {
+            return frequency.Hertz / ( 1000m * 1000m * 1000m );
+        }
+
+        public static GigaHertz ToGigaHertz( this Frequency frequency )
+        {
+            return new GigaHertz( frequency.GigaHertz() );
+        }
+    }
+}
