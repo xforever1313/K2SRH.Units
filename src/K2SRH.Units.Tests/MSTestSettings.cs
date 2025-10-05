bfa20909e9cf6cb019aa423c053d554b48db6145
Seth Hendrick <seth@shendrick.net>
2025-10-05 12:33:57 -0400

    Added license

--- src/K2SRH.Units.Tests/MSTestSettings.cs
+++ src/K2SRH.Units.Tests/MSTestSettings.cs
@@ -1,1 +1,8 @@
-﻿[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+﻿//
+//          Copyright Seth Hendrick 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
--- src/K2SRH.Units/Frequency/Frequency.cs
+++ src/K2SRH.Units/Frequency/Frequency.cs
@@ -1,4 +1,11 @@
-﻿namespace K2SRH.Units
+﻿//
+//          Copyright Seth Hendrick 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+namespace K2SRH.Units
 {
     [BaseUnit]
     public partial struct Frequency
@@ -9,7 +16,7 @@
 
         // ---------------- Constructor ----------------
 
-        public Frequency( decimal hertz )
+        public Frequency(decimal hertz)
         {
             this.value = hertz;
         }
