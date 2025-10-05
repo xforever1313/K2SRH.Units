a20fe0158a1372babac132662e9ee2288c90d6b4
Seth Hendrick <seth@shendrick.net>
2025-10-05 12:42:40 -0400

    Moved value to generated code.  Add min value and max value

--- src/K2SRH.Units/Frequency/Frequency.cs
+++ src/K2SRH.Units/Frequency/Frequency.cs
@@ -10,10 +10,6 @@ namespace K2SRH.Units
     [BaseUnit]
     public partial struct Frequency
     {
-        // ---------------- Fields ----------------
-
-        private readonly decimal value;
-
         // ---------------- Constructor ----------------
 
         public Frequency(decimal hertz)
