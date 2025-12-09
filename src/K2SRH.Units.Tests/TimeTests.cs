bb8f27d6f0163cc33d06c867e1a6eb1d4f3fb001
Seth Hendrick <seth@shendrick.net>
2025-12-08 19:00:02 -0500

    added more time tests

--- src/K2SRH.Units.Tests/TimeTests.cs
+++ src/K2SRH.Units.Tests/TimeTests.cs
@@ -6,6 +6,22 @@
         // ---------------- Tests ----------------
 
         [TestMethod]
+        public void ConvertTo1WeekTest()
+        {
+            // Setup
+            TimeSpan expected = new TimeSpan( 7, 0, 0, 0 );
+
+            // Act / Check
+            Assert.AreEqual( expected, new Microseconds( 604800000000 ) );
+            Assert.AreEqual( expected, new Milliseconds( 604800000 ) );
+            Assert.AreEqual( expected, new Seconds( 604800 ) );
+            Assert.AreEqual( expected, new Minutes( 10080 ) );
+            Assert.AreEqual( expected, new Hours( 168 ) );
+            Assert.AreEqual( expected, new Days( 7m ) );
+            Assert.AreEqual( expected, new Weeks( 1m ) );
+        }
+
+        [TestMethod]
         public void ConvertTo1DayTest()
         {
             // Setup
@@ -35,6 +51,22 @@
             Assert.AreEqual( expected, new Hours( 1 ) );
             Assert.AreEqual( expected, new Days( 1m / 24m ) );
             Assert.AreEqual( expected, new Weeks( 1m / 7m / 24m ) );
+        }
+
+        [TestMethod]
+        public void ConvertTo1MillisecondsTest()
+        {
+            // Setup
+            TimeSpan expected = new TimeSpan( 0, 0, 0, 0, 1 );
+
+            // Act / Check
+            Assert.AreEqual( expected, new Microseconds( 1000 ) );
+            Assert.AreEqual( expected, new Milliseconds( 1 ) );
+            Assert.AreEqual( expected, new Seconds( 0.001m ) );
+            Assert.AreEqual( expected, new Minutes( 0.000016666666666666666666666666666666666666666666666666666666666666666666666666666666666666m ) );
+            Assert.AreEqual( expected, new Hours( 2.7777777777777777777777777777777777777777777777777777777777777777777777777777e-7m ) );
+            //Assert.AreEqual( expected, new Days( 1.15740740740740736e-8m ) );
+            //Assert.AreEqual( expected, new Weeks( 1m / 7m / 24m ) );
         }
     }
 }
